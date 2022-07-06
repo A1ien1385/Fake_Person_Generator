8793c725d501c4c3f9152c589f48359313c6480e
A1ien1385 <damian.zawadzki@tlen.pl>
2022-07-06 15:03:23 +0200

    fetch method add

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("It works!");
+const downloadUsers = () => {
+  const url = "https://randomuser.me/api/?results=10";
+  fetch(url)
+    .then((response) => {
+      console.log(response);
+      if (response.status !== 200) {
+        throw Error("response.status !== 200");
+      } else {
+        return response.json();
+      }
+    })
+    .then((json) => console.log(json))
+    .catch((err) => console.log(err));
+};
+
+document.querySelector("button").addEventListener("click", downloadUsers);
